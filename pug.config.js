e8c73bc017fbd52e3dfea7cdb57f2ffcbd5d12ca
Arvind S <arvind0598@gmail.com>
2019-11-10 01:25:59 +0530

    refactored links data for navbar

--- pug.config.js
+++ pug.config.js
@@ -1,7 +1,9 @@
 const STORIES_DATA = require('./src/data/stories.json');
+const LINKS_DATA = require('./src/data/links.json');
 
 module.exports = {
   locals: {
     STORIES_DATA,
+    LINKS_DATA,
   },
 };
